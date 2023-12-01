391b10d4778b48f1295dba8e3ce1be138bbbe8fb
AloizioMacedo <atsam2@hotmail.com>
2023-12-01 03:31:10 -0300

    Solve part two of day 1 in Rust

--- Rust/day1/src/main.rs
+++ Rust/day1/src/main.rs
@@ -2,6 +2,13 @@ use anyhow::{anyhow, Result};
 
 const INPUT: &str = include_str!("../input.txt");
 
+const SPELLED_OUT_NUMBERS: [&str; 9] = [
+    "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+];
+
+// To avoid having to allocate Strings from the integers.
+const _ONE_TO_9: [&str; 9] = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
 fn parse_line(line: &str) -> Result<u32> {
     let mut numbers = line.chars().filter_map(|c| c.to_digit(10)).peekable();
 
@@ -15,12 +22,66 @@ fn parse_line(line: &str) -> Result<u32> {
     Ok(10 * first + last)
 }
 
+fn transform_line(line: &str) -> Vec<u32> {
+    let mut numbers = Vec::new();
+
+    // Adding gibberish at the end just to guarantee comfortable buffering.
+    let fake_line = line.to_string() + "AAAAA";
+
+    for i in 0..line.len() {
+        for (j, spelled_out_number) in SPELLED_OUT_NUMBERS.iter().enumerate() {
+            if fake_line[i..(i + 5)].starts_with(spelled_out_number) {
+                numbers.push(j as u32 + 1);
+                break;
+            }
+        }
+
+        if let Some(x) = fake_line[i..(i + 5)]
+            .chars()
+            .next()
+            .expect("Should not be empty given that it has AAAAA at the end")
+            .to_digit(10)
+        {
+            numbers.push(x);
+        }
+    }
+
+    numbers
+}
+
+/// This trick doesn't work for some reason that is still not clear.
+fn _adjust_line(line: &str) -> String {
+    let mut line = line.to_string();
+
+    for (spelled_out_number, number) in SPELLED_OUT_NUMBERS.iter().zip(_ONE_TO_9) {
+        line = line.replace(
+            spelled_out_number,
+            &(spelled_out_number.to_string() + number), // It is necessary to keep the spelled out
+                                                        // version in order not to lose chars.
+        );
+    }
+
+    line
+}
+
 fn solve_part_one(contents: &str) -> Result<u32> {
     contents.lines().map(parse_line).sum()
 }
 
+fn solve_part_two(contents: &str) -> Result<u32> {
+    contents
+        .lines()
+        .map(transform_line)
+        .map(|numbers| match (numbers.first(), numbers.last()) {
+            (Some(x), Some(y)) => Ok(10 * x + y),
+            _ => Err(anyhow!("Line with no numbers")),
+        })
+        .sum()
+}
+
 fn main() -> Result<()> {
     println!("{}", solve_part_one(INPUT)?);
+    println!("{}", solve_part_two(INPUT)?);
 
     Ok(())
 }
@@ -28,7 +89,8 @@ fn main() -> Result<()> {
 #[cfg(test)]
 mod tests {
     use super::*;
-    const TEST: &str = include_str!("../test_input.txt");
+    const TEST_P1: &str = include_str!("../test_input_p1.txt");
+    const TEST_P2: &str = include_str!("../test_input_p2.txt");
 
     #[test]
     fn parsing() {
@@ -40,6 +102,11 @@ mod tests {
 
     #[test]
     fn part_one() {
-        assert_eq!(solve_part_one(TEST).unwrap(), 142);
+        assert_eq!(solve_part_one(TEST_P1).unwrap(), 142);
+    }
+
+    #[test]
+    fn part_two() {
+        assert_eq!(solve_part_two(TEST_P2).unwrap(), 281);
     }
 }
