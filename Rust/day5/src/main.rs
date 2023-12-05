2a76aaaec02ae91e63de1244b619cbd7c195df96
AloizioMacedo <atsam2@hotmail.com>
2023-12-04 23:25:23 -0300

    Initial commit of day 5

---
+++ Rust/day5/src/main.rs
@@ -0,0 +1,24 @@
+use anyhow::Result;
+
+const INPUT: &str = include_str!("../input.txt");
+
+fn parse_contents(contents: &str) {
+    todo!()
+}
+
+fn parse_line(line: &str) {
+    todo!()
+}
+
+fn main() {
+    println!("Hello, world!");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    const TEST_INPUT: &str = include_str!("../test_input.txt");
+
+    #[test]
+    fn it_works() {}
+}
