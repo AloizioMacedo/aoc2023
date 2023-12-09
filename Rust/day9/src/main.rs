c0a38176dcd7147b75fba4f98d6034121f57522f
AloizioMacedo <atsam2@hotmail.com>
2023-12-09 03:20:13 -0300

    Solve part two of day 9 in Rust

--- Rust/day9/src/main.rs
+++ Rust/day9/src/main.rs
@@ -4,6 +4,11 @@ use anyhow::Result;
 
 const INPUT: &str = include_str!("../input.txt");
 
+enum TimeDirection {
+    Future,
+    Past,
+}
+
 struct History {
     values: VecDeque<i32>,
 }
@@ -26,33 +31,65 @@ impl History {
         Some(last_parent + last_value)
     }
 
-    fn forecast(&self) -> Option<i32> {
+    fn forecast_base_back(&self, parents: &[i32]) -> Option<i32> {
+        let last_parent = parents.first()?;
+
+        let last_value = self.values.front()?;
+
+        Some(last_parent + last_value)
+    }
+
+    fn forecast(&self, time_direction: TimeDirection) -> Option<i32> {
         let mut diffs: Vec<History> = vec![History {
             values: self.values.clone(),
         }];
 
-        while !diffs.last().unwrap().values.iter().all(|&x| x == 0) {
+        while !diffs
+            .last()
+            .expect("Diffs have at least the initial history")
+            .values
+            .iter()
+            .all(|&x| x == 0)
+        {
             let last_diffs = diffs.last()?;
             let values = last_diffs.get_diffs();
 
             diffs.push(History { values })
         }
 
-        diffs.last_mut().map(|x| x.values.push_back(0));
+        match time_direction {
+            TimeDirection::Future => diffs.last_mut().map(|x| x.values.push_back(0)),
+            TimeDirection::Past => diffs.last_mut().map(|x| x.values.push_front(0)),
+        };
 
         for i in 0..(diffs.len() - 1) {
             let idx = diffs.len() - i - 1;
             let seeing = &diffs[idx].values;
 
-            let last_from_seeing = *seeing.back()?;
-            let last_from_parents = *diffs[idx - 1].values.back()?;
+            match time_direction {
+                TimeDirection::Future => {
+                    let last_from_seeing = *seeing.back()?;
+                    let last_from_parents = *diffs[idx - 1].values.back()?;
 
-            diffs[idx - 1]
-                .values
-                .push_back(last_from_seeing + last_from_parents);
+                    diffs[idx - 1]
+                        .values
+                        .push_back(last_from_parents + last_from_seeing);
+                }
+                TimeDirection::Past => {
+                    let last_from_seeing = *seeing.front()?;
+                    let last_from_parents = *diffs[idx - 1].values.front()?;
+
+                    diffs[idx - 1]
+                        .values
+                        .push_front(last_from_parents - last_from_seeing);
+                }
+            }
         }
 
-        diffs[0].values.back().map(|x| *x)
+        match time_direction {
+            TimeDirection::Future => diffs[0].values.back().map(|x| *x),
+            TimeDirection::Past => diffs[0].values.front().map(|x| *x),
+        }
     }
 }
 
@@ -71,12 +108,22 @@ fn solve_part_one(contents: &str) -> Result<i32> {
 
     Ok(histories
         .iter()
-        .flat_map(|history| history.forecast())
+        .flat_map(|history| history.forecast(TimeDirection::Future))
+        .sum())
+}
+
+fn solve_part_two(contents: &str) -> Result<i32> {
+    let histories = parse_contents(contents)?;
+
+    Ok(histories
+        .iter()
+        .flat_map(|history| history.forecast(TimeDirection::Past))
         .sum())
 }
 
 fn main() -> Result<()> {
     println!("{}", solve_part_one(INPUT)?);
+    println!("{}", solve_part_two(INPUT)?);
 
     Ok(())
 }
@@ -89,5 +136,10 @@ mod tests {
     #[test]
     fn part_one() {
         assert_eq!(solve_part_one(TEST_INPUT).unwrap(), 114)
+    }
+
+    #[test]
+    fn part_two() {
+        assert_eq!(solve_part_two("10  13  16  21  30  45").unwrap(), 5)
     }
 }
