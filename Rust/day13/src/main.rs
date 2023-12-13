918a44e137e0e4dbc3e88316853f10acd044d0eb
AloizioMacedo <atsam2@hotmail.com>
2023-12-13 03:30:34 -0300

    Solve part two of day 13 in Rust

--- Rust/day13/src/main.rs
+++ Rust/day13/src/main.rs
@@ -1,7 +1,6 @@
 use anyhow::{anyhow, Result};
 use ndarray::Array2;
 
-const TEST_INPUT: &str = include_str!("../test_input.txt");
 const INPUT: &str = include_str!("../input.txt");
 
 fn parse_contents(contents: &str) -> Result<Vec<Array2<char>>> {
@@ -74,10 +73,90 @@ fn solve_part_one(contents: &str) -> Result<usize> {
     Ok(total)
 }
 
-fn solve_part_two() {}
+enum Reflection {
+    Row(usize),
+    Column(usize),
+}
+
+fn get_rows_and_columns_from_part_one(contents: &str) -> Result<Vec<Reflection>> {
+    let matrices = parse_contents(contents)?;
+
+    let mut reflections = Vec::new();
+    for matrix in matrices {
+        for j in 0..(matrix.ncols() - 1) {
+            if is_reflection_across_col(&matrix, j) {
+                reflections.push(Reflection::Column(j));
+            }
+        }
+
+        for i in 0..(matrix.nrows() - 1) {
+            if is_reflection_across_row(&matrix, i) {
+                reflections.push(Reflection::Row(i));
+            }
+        }
+    }
+
+    Ok(reflections)
+}
+
+fn flip(matrix: &Array2<char>, i: usize, j: usize) -> Array2<char> {
+    let mut matrix = matrix.clone();
+
+    match matrix[(i, j)] {
+        '#' => matrix[(i, j)] = '.',
+        '.' => matrix[(i, j)] = '#',
+        _ => panic!("Flipping a matrix that is ill-formed to the problem"),
+    }
+
+    matrix
+}
+
+fn solve_part_two(contents: &str) -> Result<usize> {
+    let matrices = parse_contents(contents)?;
+    let rows_and_cols = get_rows_and_columns_from_part_one(contents)?;
+
+    let mut total = 0;
+
+    'outer: for (matrix, row_or_col) in matrices.iter().zip(rows_and_cols) {
+        for i1 in 0..matrix.nrows() {
+            for j1 in 0..matrix.ncols() {
+                let matrix = flip(matrix, i1, j1);
+
+                for j in 0..(matrix.ncols() - 1) {
+                    if let Reflection::Column(c) = row_or_col {
+                        if c == j {
+                            continue;
+                        }
+                    }
+
+                    if is_reflection_across_col(&matrix, j) {
+                        total += j + 1;
+                        continue 'outer;
+                    }
+                }
+
+                for i in 0..(matrix.nrows() - 1) {
+                    if let Reflection::Row(r) = row_or_col {
+                        if r == i {
+                            continue;
+                        }
+                    }
+
+                    if is_reflection_across_row(&matrix, i) {
+                        total += 100 * (i + 1);
+                        continue 'outer;
+                    }
+                }
+            }
+        }
+    }
+
+    Ok(total)
+}
 
 fn main() -> Result<()> {
     println!("{}", solve_part_one(INPUT)?);
+    println!("{}", solve_part_two(INPUT)?);
 
     Ok(())
 }
@@ -90,5 +169,10 @@ mod tests {
     #[test]
     fn part_one() {
         assert_eq!(solve_part_one(TEST_INPUT).unwrap(), 405);
+    }
+
+    #[test]
+    fn part_two() {
+        assert_eq!(solve_part_two(TEST_INPUT).unwrap(), 400);
     }
 }
