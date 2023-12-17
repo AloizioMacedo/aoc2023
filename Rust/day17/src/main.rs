6556aec0f4ebae852d5e4a62490f207319c9d514
AloizioMacedo <atsam2@hotmail.com>
2023-12-17 04:10:07 -0300

    Solve part one of day 17 in Rust

--- Rust/day17/src/main.rs
+++ Rust/day17/src/main.rs
@@ -6,7 +6,7 @@ use ndarray::Array2;
 const TEST: &str = include_str!("../test_input.txt");
 const INPUT: &str = include_str!("../input.txt");
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord)]
 enum Direction {
     Up,
     Right,
@@ -49,8 +49,6 @@ impl Direction {
     }
 }
 
-fn parse_line(line: &str) {}
-
 fn parse_contents(contents: &str) -> Result<Array2<u32>> {
     let lines: Vec<&str> = contents.lines().collect();
 
@@ -67,169 +65,164 @@ fn parse_contents(contents: &str) -> Result<Array2<u32>> {
     Ok(Array2::from_shape_vec((nrows, ncols), contents)?)
 }
 
-fn get_distance(matrix: &Array2<u32>) -> usize {
-    let destination = (matrix.nrows() - 1, matrix.ncols() - 1);
+fn get_distance(matrix: &Array2<u32>) -> Result<i32> {
+    let mut distances: HashMap<VisitedWithSteps, i32> = HashMap::new();
+    for i in 0..matrix.nrows() {
+        for j in 0..matrix.ncols() {
+            for k in 0..4 {
+                distances.insert(
+                    VisitedWithSteps {
+                        node: (i, j),
+                        steps: k,
+                        direction: Direction::Up,
+                    },
+                    i32::MAX,
+                );
+                distances.insert(
+                    VisitedWithSteps {
+                        node: (i, j),
+                        steps: k,
+                        direction: Direction::Right,
+                    },
+                    i32::MAX,
+                );
+                distances.insert(
+                    VisitedWithSteps {
+                        node: (i, j),
+                        steps: k,
+                        direction: Direction::Down,
+                    },
+                    i32::MAX,
+                );
+                distances.insert(
+                    VisitedWithSteps {
+                        node: (i, j),
+                        steps: k,
+                        direction: Direction::Left,
+                    },
+                    i32::MAX,
+                );
+            }
+        }
+    }
 
-    let mut distances: HashMap<(usize, usize), u32> =
-        HashMap::from_iter(matrix.indexed_iter().map(|((i, j), _)| ((i, j), u32::MAX)));
-    distances.insert((0, 0), 0);
+    distances.insert(
+        VisitedWithSteps {
+            node: (0, 0),
+            steps: 0,
+            direction: Direction::Right,
+        },
+        0,
+    );
 
-    // let mut distances: HashMap<VisitedWithSteps, u32> = HashMap::new();
-    // for i in 0..matrix.nrows() {
-    //     for j in 0..matrix.ncols() {
-    //         for k in 0..4 {
-    //             distances.insert(
-    //                 VisitedWithSteps {
-    //                     node: (i, j),
-    //                     steps: k,
-    //                     direction: Direction::Up,
-    //                     distance: u32::MAX,
-    //                 },
-    //                 u32::MAX,
-    //             )
-    //         }
-    //         distances.insert(
-    //             VisitedWithSteps {
-    //                 node: (i, j),
-    //                 steps: 0,
-    //                 direction: Direction::Right,
-    //                 distance: u32::MAX,
-    //             },
-    //             u32::MAX,
-    //         );
-    //     }
-    // }
-
-    // distances.insert(
-    //     VisitedWithSteps {
-    //         node: (0, 0),
-    //         steps: 0,
-    //         direction: Direction::Right,
-    //         distance: 0,
-    //     },
-    //     0,
-    // );
-
-    let mut queue: BinaryHeap<VisitedWithSteps> = BinaryHeap::new();
-    queue.push(VisitedWithSteps {
-        node: (0, 0),
-        steps: 0,
-        direction: Direction::Right,
-        distance: 0,
-    });
+    let mut queue: BinaryHeap<(i32, VisitedWithSteps)> = BinaryHeap::new();
+    queue.push((
+        0,
+        VisitedWithSteps {
+            node: (0, 0),
+            steps: 0,
+            direction: Direction::Right,
+        },
+    ));
 
     let mut visited = HashSet::new();
 
-    while let Some(current) = queue.pop() {
+    while let Some((_, current)) = queue.pop() {
         if !visited.insert(current) {
             continue;
         };
         let next_directions = current.direction.next_directions(current.steps);
 
         for (direction, steps) in next_directions {
-            match (direction, steps) {
-                (Direction::Up, steps) => {
+            let coords = match (direction, steps) {
+                (Direction::Up, _) => {
                     if current.node.0 > 0 {
                         let i = current.node.0 - 1;
                         let j = current.node.1;
-
-                        let distance = distances[&(i, j)].min(current.distance + matrix[(i, j)]);
-                        distances.insert((i, j), distance);
-                        queue.push(VisitedWithSteps {
-                            node: (i, j),
-                            steps,
-                            direction,
-                            distance,
-                        });
+                        Some((i, j))
+                    } else {
+                        None
                     }
                 }
-                (Direction::Right, steps) => {
+                (Direction::Right, _) => {
                     if current.node.1 < matrix.ncols() - 1 {
                         let i = current.node.0;
                         let j = current.node.1 + 1;
-
-                        let distance = distances[&(i, j)].min(current.distance + matrix[(i, j)]);
-                        distances.insert((i, j), distance);
-                        queue.push(VisitedWithSteps {
-                            node: (i, j),
-                            steps,
-                            direction,
-                            distance,
-                        });
+                        Some((i, j))
+                    } else {
+                        None
                     }
                 }
-                (Direction::Down, steps) => {
+                (Direction::Down, _) => {
                     if current.node.0 < matrix.nrows() - 1 {
                         let i = current.node.0 + 1;
                         let j = current.node.1;
-
-                        let distance = distances[&(i, j)].min(current.distance + matrix[(i, j)]);
-                        distances.insert((i, j), distance);
-                        queue.push(VisitedWithSteps {
-                            node: (i, j),
-                            steps,
-                            direction,
-                            distance,
-                        });
+                        Some((i, j))
+                    } else {
+                        None
                     }
                 }
-                (Direction::Left, steps) => {
+                (Direction::Left, _) => {
                     if current.node.1 > 0 {
                         let i = current.node.0;
                         let j = current.node.1 - 1;
-
-                        let distance = distances[&(i, j)].min(current.distance + matrix[(i, j)]);
-                        distances.insert((i, j), distance);
-                        queue.push(VisitedWithSteps {
-                            node: (i, j),
-                            steps,
-                            direction,
-                            distance,
-                        });
+                        Some((i, j))
+                    } else {
+                        None
                     }
                 }
+            };
+
+            if let Some((i, j)) = coords {
+                let distance = distances[&VisitedWithSteps {
+                    node: (i, j),
+                    steps,
+                    direction,
+                }]
+                    .min(distances[&current] + matrix[(i, j)] as i32);
+                distances.insert(
+                    VisitedWithSteps {
+                        node: (i, j),
+                        steps,
+                        direction,
+                    },
+                    distance,
+                );
+                queue.push((
+                    -distance,
+                    VisitedWithSteps {
+                        node: (i, j),
+                        steps,
+                        direction,
+                    },
+                ));
             }
-        }
-        if distances[&destination] < u32::MAX {
-            break;
         }
     }
 
-    distances[&destination] as usize
+    distances
+        .iter()
+        .filter(|(k, _)| k.node == (matrix.nrows() - 1, matrix.ncols() - 1))
+        .map(|(_, v)| *v)
+        .min()
+        .ok_or(anyhow!("Not able to reach destination"))
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord)]
 struct VisitedWithSteps {
     node: (usize, usize),
     steps: usize,
     direction: Direction,
-    distance: u32,
 }
 
-impl PartialOrd for VisitedWithSteps {
-    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
-impl Ord for VisitedWithSteps {
-    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        other.distance.cmp(&self.distance)
-    }
-}
-
-fn solve_part_one(contents: &str) -> Result<usize> {
+fn solve_part_one(contents: &str) -> Result<i32> {
     let matrix = parse_contents(contents)?;
 
-    let distance = get_distance(&matrix);
-
-    Ok(distance)
+    get_distance(&matrix)
 }
 
-fn solve_part_two(contents: &str) {}
-
 fn main() -> Result<()> {
-    println!("{}", solve_part_one(TEST)?);
+    println!("{}", solve_part_one(INPUT)?);
 
     Ok(())
 }
@@ -239,24 +232,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        println!("{}", solve_part_one(TEST).unwrap());
-    }
-
-    #[test]
-    fn inequality() {
-        assert!(
-            VisitedWithSteps {
-                node: (0, 0),
-                steps: 3,
-                direction: Direction::Right,
-                distance: 5
-            } > VisitedWithSteps {
-                node: (0, 0),
-                steps: 3,
-                direction: Direction::Right,
-                distance: 12
-            }
-        );
+    fn part_one() {
+        assert_eq!(solve_part_one(TEST).unwrap(), 102);
     }
 }
