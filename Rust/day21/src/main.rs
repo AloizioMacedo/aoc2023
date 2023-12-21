381828f262610473c012f284ddb0347bc6e27dc5
AloizioMacedo <atsam2@hotmail.com>
2023-12-21 16:26:32 -0300

    Start part two of day 21

--- Rust/day21/src/main.rs
+++ Rust/day21/src/main.rs
@@ -1,4 +1,4 @@
-use std::{collections::VecDeque, thread::current};
+use std::collections::{HashSet, VecDeque};
 
 use anyhow::{anyhow, Result};
 use ndarray::Array2;
@@ -6,7 +6,35 @@ use ndarray::Array2;
 const TEST: &str = include_str!("../test_input.txt");
 const INPUT: &str = include_str!("../input.txt");
 
-fn parse_line(line: &str) {}
+#[derive(Debug, Clone)]
+struct Matrix {
+    base: Array2<char>,
+    steps: HashSet<(i32, i32)>,
+}
+
+impl Matrix {
+    fn get(&self, tup: (i32, i32)) -> char {
+        if self.steps.contains(&tup) {
+            return 'O';
+        }
+
+        let nrows = self.base.nrows();
+        let ncols = self.base.ncols();
+
+        self.base[(
+            tup.0.rem_euclid(nrows as i32) as usize,
+            tup.1.rem_euclid(ncols as i32) as usize,
+        )]
+    }
+
+    fn set(&mut self, tup: (i32, i32), c: char) {
+        if let 'O' = c {
+            self.steps.insert(tup);
+        } else {
+            self.steps.remove(&tup);
+        }
+    }
+}
 
 fn parse_contents(contents: &str) -> Result<Array2<char>> {
     let lines = contents.lines().collect::<Vec<_>>();
@@ -17,6 +45,90 @@ fn parse_contents(contents: &str) -> Result<Array2<char>> {
     let flat_matrix = lines.iter().flat_map(|x| x.chars()).collect::<Vec<_>>();
 
     Ok(Array2::from_shape_vec((nrows, ncols), flat_matrix)?)
+}
+
+fn parse_contents_p2(contents: &str) -> Result<Matrix> {
+    let lines = contents.lines().collect::<Vec<_>>();
+
+    let nrows = lines.len();
+    let ncols = lines[0].len();
+
+    let flat_matrix = lines.iter().flat_map(|x| x.chars()).collect::<Vec<_>>();
+
+    let base = Array2::from_shape_vec((nrows, ncols), flat_matrix)?;
+    let steps = HashSet::new();
+
+    Ok(Matrix { base, steps })
+}
+
+fn bfs_matrix(matrix: &Matrix, steps: usize) -> Result<usize> {
+    let mut matrix = matrix.clone();
+
+    let beginning = matrix
+        .base
+        .indexed_iter()
+        .find(|(_, &v)| v == 'S')
+        .ok_or(anyhow!("No 'S'"))?
+        .0;
+
+    matrix.base[beginning] = 'O';
+
+    matrix.set((beginning.0 as i32, beginning.1 as i32), 'O');
+
+    let mut current_queue = VecDeque::new();
+    let mut next_queue = VecDeque::new();
+
+    current_queue.push_front((beginning.0 as i32, beginning.1 as i32));
+
+    let mut counter = 0;
+    while let Some((i, j)) = current_queue.pop_back() {
+        if counter >= steps {
+            break;
+        }
+
+        //eprintln!(
+        //"Current: {:?}, Steps before proceeding: {:?}",
+        //(i, j),
+        //matrix.steps
+        //);
+
+        matrix.set((i, j), '.');
+        // eprintln!(
+        //"North: {:?}, South: {:?}, West: {:?}, East: {:?}",
+        //matrix.get((i - 1, j)),
+        //matrix.get((i + 1, j)),
+        //matrix.get((i, j - 1)),
+        //matrix.get((i, j + 1)),
+        //);
+
+        if matrix.get((i - 1, j)) == '.' || matrix.get((i - 1, j)) == 'O' {
+            matrix.set((i - 1, j), 'O');
+            next_queue.push_front((i - 1, j));
+        }
+        if matrix.get((i + 1, j)) == '.' || matrix.get((i + 1, j)) == 'O' {
+            matrix.set((i + 1, j), 'O');
+            next_queue.push_front((i + 1, j));
+        }
+        if matrix.get((i, j - 1)) == '.' || matrix.get((i, j - 1)) == 'O' {
+            matrix.set((i, j - 1), 'O');
+            next_queue.push_front((i, j - 1));
+        }
+        if matrix.get((i, j + 1)) == '.' || matrix.get((i, j + 1)) == 'O' {
+            matrix.set((i, j + 1), 'O');
+            next_queue.push_front((i, j + 1));
+        }
+
+        // eprintln!("Steps after proceeding: {:?}", matrix.steps);
+
+        if current_queue.is_empty() {
+            current_queue.extend(&next_queue);
+            next_queue.clear();
+
+            counter += 1;
+        }
+    }
+
+    Ok(matrix.steps.len())
 }
 
 fn bfs(matrix: &Array2<char>, steps: usize) -> Result<usize> {
@@ -78,8 +190,24 @@ fn solve_part_one(contents: &str, n: usize) -> Result<usize> {
     bfs(&matrix, n)
 }
 
+fn solve_part_two(contents: &str, n: usize) -> Result<usize> {
+    let matrix = parse_contents_p2(contents)?;
+    let nrows = matrix.base.nrows();
+
+    for i in 1..n {
+        // if i % nrows == 26_501_365 % nrows {
+        println!("{i}: {}", bfs_matrix(&matrix, i)?);
+        //}
+    }
+
+    bfs_matrix(&matrix, n)
+}
+
 fn main() -> Result<()> {
     println!("{}", solve_part_one(INPUT, 64)?);
+    // println!("{}", solve_part_two(TEST, 5)?);
+    println!("{}", solve_part_two(TEST, 26_501_365)?);
+    // println!("{}", solve_part_one(INPUT, 26_501_365)?);
 
     Ok(())
 }
