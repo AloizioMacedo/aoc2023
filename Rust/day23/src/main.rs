87c6e31d32a16a2f6900edd7f1c0e4382537d9e1
AloizioMacedo <atsam2@hotmail.com>
2023-12-23 04:31:07 -0300

    Solve part two of day 23 in Rust
    Optimize this later

--- Rust/day23/src/main.rs
+++ Rust/day23/src/main.rs
@@ -86,6 +86,70 @@ fn bfs(matrix: &Array2<char>, origin: (usize, usize), destination: (usize, usize
     greatest_path_length
 }
 
+fn bfs_no_slope(
+    matrix: &Array2<char>,
+    origin: (usize, usize),
+    destination: (usize, usize),
+) -> usize {
+    let mut queue = VecDeque::new();
+
+    let nrows = matrix.nrows();
+    let ncols = matrix.ncols();
+
+    let mut visited = HashSet::new();
+    visited.insert(origin);
+    queue.push_front((origin, visited));
+
+    let mut greatest_path_length = 0;
+    let mut previous_grt_path_length = 0;
+
+    while let Some(((i, j), p)) = queue.pop_back() {
+        if i > 0 && !p.contains(&(i - 1, j)) && matrix[(i - 1, j)] != '#' {
+            if (i - 1, j) == destination {
+                greatest_path_length = greatest_path_length.max(p.len());
+            } else {
+                let mut p = p.clone();
+                p.insert((i - 1, j));
+                queue.push_back(((i - 1, j), p));
+            }
+        }
+        if i < nrows - 1 && !p.contains(&(i + 1, j)) && matrix[(i + 1, j)] != '#' {
+            if (i + 1, j) == destination {
+                greatest_path_length = greatest_path_length.max(p.len());
+            } else {
+                let mut p = p.clone();
+                p.insert((i + 1, j));
+                queue.push_back(((i + 1, j), p));
+            }
+        }
+        if j > 0 && !p.contains(&(i, j - 1)) && matrix[(i, j - 1)] != '#' {
+            if (i, j - 1) == destination {
+                greatest_path_length = greatest_path_length.max(p.len());
+            } else {
+                let mut p = p.clone();
+                p.insert((i, j - 1));
+                queue.push_back(((i, j - 1), p));
+            }
+        }
+        if j < ncols - 1 && !p.contains(&(i, j + 1)) && matrix[(i, j + 1)] != '#' {
+            if (i, j + 1) == destination {
+                greatest_path_length = greatest_path_length.max(p.len());
+            } else {
+                let mut p = p.clone();
+                p.insert((i, j + 1));
+                queue.push_back(((i, j + 1), p));
+            }
+        }
+
+        if previous_grt_path_length < greatest_path_length {
+            println!("{}", greatest_path_length);
+            previous_grt_path_length = greatest_path_length;
+        }
+    }
+
+    greatest_path_length
+}
+
 fn solve_part_one(contents: &str) -> Result<usize> {
     let matrix = parse_contents(contents)?;
     let nrows = matrix.nrows();
@@ -94,8 +158,17 @@ fn solve_part_one(contents: &str) -> Result<usize> {
     Ok(bfs(&matrix, (0, 1), (nrows - 1, ncols - 2)))
 }
 
+fn solve_part_two(contents: &str) -> Result<usize> {
+    let matrix = parse_contents(contents)?;
+    let nrows = matrix.nrows();
+    let ncols = matrix.ncols();
+
+    Ok(bfs_no_slope(&matrix, (0, 1), (nrows - 1, ncols - 2)))
+}
+
 fn main() -> Result<()> {
     println!("{:?}", solve_part_one(INPUT)?);
+    println!("{:?}", solve_part_two(INPUT)?);
 
     Ok(())
 }
@@ -107,5 +180,10 @@ mod tests {
     #[test]
     fn part_one() {
         assert_eq!(solve_part_one(TEST).unwrap(), 94);
+    }
+
+    #[test]
+    fn part_two() {
+        assert_eq!(solve_part_two(TEST).unwrap(), 154);
     }
 }
